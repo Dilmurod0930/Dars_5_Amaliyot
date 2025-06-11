4e490558fe3376f43c491233205d9db0b28a5618
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 14:58:23 +0500

    Updage  fayl  qo'shildi.

--- telefon_App/urls.py
+++ telefon_App/urls.py
@@ -1,9 +1,11 @@
 from  django.urls import path
-from  .views import tele_list, tele_info, tele_create ,, tele_delete
+from  .views import tele_list, tele_info, tele_create ,tele_delete , tele_update
 
 urlpatterns =[
     path('tele_list', tele_list, name='tele_list'),
     path('tele_info/<int:id>', tele_info, name='tele_info'),
     path('tele_create', tele_create, name='tele_create'),
     path('tele_delete/<int:id>', tele_delete, name='tele_delete'),
+    path('tele_update/<int:id>', tele_update, name='tele_update'),
+
 ]
