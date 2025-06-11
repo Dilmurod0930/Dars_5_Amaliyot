dd6b9c024c646cce83a356b70dc133ae6549a5af
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 11:15:55 +0500

    Telefon  Admin  hamda  model malumotlar  qo'shildi

---
+++ telefon_App/urls.py
@@ -0,0 +1,7 @@
+from  django.urls import path
+from  .views import tele_info
+
+urlpatterns =[
+    path('tele_info', tele_info, name='tele_info'),
+    
+]
--- telefon_App/views.py
+++ telefon_App/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def  tele_info(request):
+    return render(request, 'telefon/tele_info.html')
