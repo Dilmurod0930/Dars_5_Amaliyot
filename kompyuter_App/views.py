2e764b1a6856487e36237b786d095ab6b8669a46
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 12:18:41 +0500

    Telefon list path tugatildi.

--- kompyuter_App/views.py
+++ kompyuter_App/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import Kompyuter
 # Create your views here.
 def  main(request):
     return render(request, 'main.html')
 
 
 def  kom_list(request):
-    return render(request, 'kompyuter/kom_list.html')
+    kompyuters = Kompyuter.objects.all()
+    context = {'kompyuters': kompyuters}
+    return render(request, 'kompyuter/kom_list.html', context = context)
