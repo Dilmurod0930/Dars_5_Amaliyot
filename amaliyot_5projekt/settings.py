5d7740e8b0491e755f2b8f5e1608f66fcee8e316
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 10:23:10 +0500

    Model  qo'shildi

--- amaliyot_5projekt/settings.py
+++ amaliyot_5projekt/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kompyuter_App', 
+    'kompyuter_App',
 ]
 
 MIDDLEWARE = [
