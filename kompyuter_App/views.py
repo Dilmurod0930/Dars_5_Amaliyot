1a86839d98e857458b581a049c80a31de6687f4a
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 12:48:26 +0500

    Kompyuter  Info  page  uchun  path  qo'shildi.

--- kompyuter_App/views.py
+++ kompyuter_App/views.py
@@ -9,3 +9,8 @@ def  kom_list(request):
     kompyuters = Kompyuter.objects.all()
     context = {'kompyuters': kompyuters}
     return render(request, 'kompyuter/kom_list.html', context = context)
+
+def kom_info(request,  id):
+    kompyuter = Kompyuter.objects.get(id=id)
+    context = {'kompyuter': kompyuter}
+    return render(request, 'kompyuter/kom_info.html', context = context)
