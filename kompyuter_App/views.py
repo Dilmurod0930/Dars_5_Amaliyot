a7daf434a3a5e22eb41592a34515332a36a34a69
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 13:07:57 +0500

    Crate  fayl  yaratildi  Kompyuter  uchun.

--- kompyuter_App/views.py
+++ kompyuter_App/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect
 from .models import Kompyuter
 # Create your views here.
 def  main(request):
@@ -14,3 +14,34 @@ def kom_info(request,  id):
     kompyuter = Kompyuter.objects.get(id=id)
     context = {'kompyuter': kompyuter}
     return render(request, 'kompyuter/kom_info.html', context = context)
+
+def  kom_create(request):
+    """ name = models.CharField("Kompyuter nomi", max_length=100)
+    model = models.CharField("Model", max_length=100)
+    cpu = models.CharField("Protsessor (CPU)", max_length=100)
+    ram = models.IntegerField("RAM (GB)")
+    storage = models.CharField("Xotira turi va hajmi", max_length=100)
+    screen_size = models.DecimalField("Ekran o‘lchami (dyuym)", max_digits=4, decimal_places=2)
+    color = models.CharField("Rangi", max_length=50)
+    price = models.DecimalField("Narxi (so‘m)", max_digits=12, decimal_places=2)
+    discount_price = models.DecimalField("Chegirma narxi (so‘m)", max_digits=12, decimal_places=2, null=True, blank=True)
+    image = models.ImageField("Rasm",upload_to= "kompyuter_App/",  null=True, blank=True)
+    description = models.TextField("Tavsif", blank=True, null=True)
+    created_at = models.DateTimeField("Qo‘shilgan vaqt", auto_now_add=True)
+    updated_at = models.DateTimeField("Yangilangan vaqt", auto_now=True)"""
+    if  request.method == 'POST':
+        kompyuter = Kompyuter()
+        kompyuter.name = request.POST['name']
+        kompyuter.model = request.POST['model']
+        kompyuter.cpu = request.POST['cpu']
+        kompyuter.ram = request.POST['ram']
+        kompyuter.storage = request.POST['storage']
+        kompyuter.screen_size = request.POST['screen_size']
+        kompyuter.color = request.POST['color']
+        kompyuter.price = request.POST['price']
+        kompyuter.discount_price = request.POST['discount_price']
+        kompyuter.image = request.FILES['image']
+        kompyuter.description = request.POST['description']
+        kompyuter.save()
+        return  redirect("kom_list")
+    return render(request, 'kompyuter/kom_form.html', {'kompyuter': 'kompyuter'})
