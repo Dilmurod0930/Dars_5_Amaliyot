83d6fbcd342b2f587ab51ea4038c8ec2bd3ed05a
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 11:57:48 +0500

    Telefon  Info  page  bog'landi .

--- amaliyot_5projekt/urls.py
+++ amaliyot_5projekt/urls.py
@@ -22,6 +22,7 @@ from  django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kompyuter_App.urls')),
+    path('', include('telefon_App.urls')),
 ]
 
 if  settings.DEBUG:
--- telefon_App/views.py
+++ telefon_App/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from  .models import  Telefon
 
 # Create your views here.
 def  tele_info(request):
-    return render(request, 'telefon/tele_info.html')
+    telefons = Telefon.objects.all()
+    context = {'telefons': telefons}
+    return render(request, 'telefon/tele_info.html', context = context)
