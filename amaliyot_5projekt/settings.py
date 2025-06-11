81914a8218cce1467db7c3fabd387dcc01e00a96
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 10:28:05 +0500

    Database  settings.py  ga  bog'landi

--- amaliyot_5projekt/settings.py
+++ amaliyot_5projekt/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'amaliyot_5projekt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'kompyuter',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
