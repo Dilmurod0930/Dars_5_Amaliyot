c31913add4355ef9d30b5bfc444a878a07660a3b
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 14:36:19 +0500

    telefon info  sahifaga  finksiya  yaratildi.

--- telefon_App/views.py
+++ telefon_App/views.py
@@ -6,3 +6,8 @@ def  tele_list(request):
     telefons = Telefon.objects.all()
     context = {'telefons': telefons}
     return render(request, 'telefon/tele_list.html', context = context)
+
+def  tele_info(request, id):
+    telefon = Telefon.objects.get(id=id)
+    context = {'telefon': telefon}
+    return render(request, 'telefon/tele_info.html', context = context)
