3f38a7a63d5386580be35aa491218d1ae4110ea1
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 14:57:17 +0500

    crate  pathga  bog'landi

--- telefon_App/urls.py
+++ telefon_App/urls.py
@@ -1,7 +1,8 @@
 from  django.urls import path
-from  .views import tele_list, tele_info
+from  .views import tele_list, tele_info, tele_create ,, tele_delete
 
 urlpatterns =[
     path('tele_list', tele_list, name='tele_list'),
     path('tele_info/<int:id>', tele_info, name='tele_info'),
+    path('tele_create', tele_create, name='tele_create'),
 ]
