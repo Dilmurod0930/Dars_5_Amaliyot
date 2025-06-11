a68d81667dc09437103718d417a133ff9df0f6a6
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-11 13:27:51 +0500

    Update  page  yaratildi.

--- kompyuter_App/urls.py
+++ kompyuter_App/urls.py
@@ -1,9 +1,11 @@
 from   django.urls import path
-from  .views import main,  kom_list, kom_info,  kom_create
+from  .views import main,  kom_list, kom_info,  kom_create, kom_delete
+
 
 urlpatterns = [
     path('', main , name='main'),
     path('kom_list/', kom_list , name='kom_list'),
     path('kom_info/<int:id>/', kom_info , name='kom_info'),
     path('kom_create/', kom_create , name='kom_create'),
+    path('kom_delete/<int:id>/', kom_delete , name='kom_delete'),
 ]
